a8e25391e0aa49748acabf854c09a687e5376e5e
Noah Rontard <nrontard@c2r4p4.42perpignan.fr>
2025-02-24 16:16:52 +0100

    end of first day

---
+++ minishell.h
@@ -0,0 +1,10 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
---
+++ parsing.c
@@ -0,0 +1,29 @@
+#include "minishell.h"
+
+char	*check_path(char **env, char *cmd)
+{
+	int		i;
+	char	**path;
+	char	*temp;
+
+	if (ft_strchr(cmd, '/') != NULL)
+	{
+		if (access(cmd, F_OK) == 0)
+			return (cmd);
+	}
+	i = 0;
+	while (ft_strncmp(env[i], "PATH", 4) != 0)
+		i++;
+	path = ft_split(env[i] + 5, ':');
+	i = 0;
+	while (path[i] != NULL)
+	{
+		temp = ft_strnjoin(2, (char *[]){path[i], cmd}, "/");
+		if (access(temp, F_OK) == 0)
+			return (free_split(path), temp);
+		free(temp);
+		i++;
+	}
+	return (free_split(path), NULL);
+}
+
