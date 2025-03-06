5f9ef87ce7bfd5b2433d6d8c7d8c637c60fcc7ee
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-03-06 14:23:57 +0100

    new main

--- main.c
+++ main.c
@@ -9,24 +9,32 @@ int	main(int argc, char **argv, char **env)
 	(void) argc;
 	(void) argv;
 	var = NULL;
+	var = init_struct(var, env);
 	while (1)
 	{
 		line = readline("\033[1;33mBrioShell>\033[0m");
-		if (ft_strncmp(line, "exit", 3) == 0)
+		if (line != NULL && ft_strcmp(line, "") != 0)
 		{
-			ft_lstclear(&(var->updt_env), free);
-			ft_lstclear(&(var->export), free);
-			ft_lstclear(&(var->parse), free);
-			free(var);
-			rl_clear_history();
-			return (0);
+			if (ft_strncmp(line, "exit", 3) == 0)
+			{
+				if (var->updt_env != NULL)
+					ft_lstclear(&(var->updt_env), free);
+				if (var->export != NULL)
+					ft_lstclear(&(var->export), free);
+				if (var->parse != NULL)
+					ft_lstclear(&(var->parse), free);
+				free(var);
+				rl_clear_history();
+				return (0);
+			}
+			
+			add_history(line);
+			var->parse = parsing_line(line);
+			var->data = convert_parse(var->parse);
+			ft_cmd(var, var->data);
+			free_split(var->data);
 		}
-		add_history(line);
-		var = init_struct(var, env);
-		var->parse = parsing_line(line);
-		var->data = convert_parse(var->parse);
-		ft_cmd(var, var->data);
-		free_split(var->data);
+		free(line);
 		i = 0;
 	}
 	return (0);
