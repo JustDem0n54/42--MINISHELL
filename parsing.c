a8e358fc5382f04fb20f0c564f30c10015da02b3
Noah Rontard <nrontard@c2r4p4.42perpignan.fr>
2025-03-11 13:15:08 +0100

    propre

--- parsing.c
+++ parsing.c
@@ -200,31 +200,4 @@ char	**convert_parse(t_list *lst)
 	return(sortie);
 }
 
-// char	*check_cmd(char **env, char *cmd)
-// {
-// 	int		i;
-// 	char	**path;
-// 	char	*temp;
-	
-// 	/*check builtins*/
-// 	if (ft_strchr(cmd, '/') != NULL)
-// 	{
-// 		if (access(cmd, F_OK) == 0)
-// 			return (cmd);
-// 	}
-// 	i = 0;
-// 	while (ft_strncmp(env[i], "PATH", 4) != 0)
-// 		i++;
-// 	path = ft_split(env[i] + 5, ':');
-// 	i = 0;
-// 	while (path[i] != NULL)
-// 	{
-// 		temp = ft_strnjoin(2, (char *[]){path[i], cmd}, "/");
-// 		if (access(temp, F_OK) == 0)
-// 			return (free_split(path), temp);
-// 		free(temp);
-// 		i++;
-// 	}
-// 	return (free_split(path), NULL);
-// }
 
