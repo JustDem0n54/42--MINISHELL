b249f214cef90bde8ec4ebcebdf73fb00159345a
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-03-25 11:26:27 +0100

    signal updated

--- src/execution/signaux.c
+++ src/execution/signaux.c
@@ -2,10 +2,9 @@
 
 // void	(*signal (int sig, void(*ft_signal)(int)))(int);
 
-void	ft_ctrl_c(int sig)
+void	ft_ctrl_c_parent(int sig)
 {
 	// g_ctrl = sig;
-	// signal(SIGINT, SIG_DFL);
 	if (sig == SIGINT)
 	{
 		printf("\n");
@@ -16,49 +15,38 @@ void	ft_ctrl_c(int sig)
 	return;		
 }
 
-// void	ft_ctrl_slash_parent(int sig)
-// {
-// 	// if (sig == SIGQUIT && g_ctrl != 0)
-// 	// {
-// 	// 	printf("Quit (core dumped)\n");
-// 	// 	// printf("\n");
-// 	// 	rl_replace_line("", 0);
-// 	// 	rl_on_new_line();
-// 	// 	return;
-// 	// }
-// 	// else
-// 	// {
-// 	g_ctrl = sig;
-// 	signal(SIGQUIT, SIG_IGN);
-// 	return;
-// 	// }
-// }
-
-// void disable_ctrl_signals()
-// {
-//     struct termios term;
-
-//     // Obtenir les paramètres actuels du terminal
-//     tcgetattr(STDIN_FILENO, &term);  // STDIN_FILENO correspond à l'entrée standard (le terminal)
-
-//     // Désactiver les signaux générés00 par le terminal (Ctrl+C, Ctrl+\)
-//     term.c_lflag &= ~ISIG;  // Désactive ISIG, qui active le traitement des signaux comme Ctrl+C, Ctrl+\
-
-//     // Appliquer les modifications
-//     tcsetattr(STDIN_FILENO, TCSANOW, &term);  // Applique les changements immédiatement
-// }
-
-void	ft_ctrl_slash_child(int sig)
+void	ft_ctrl_c_child(int sig)
 {
-	printf("ok");
-	signal(SIGQUIT, SIG_DFL);
-	printf("Quit (core dumped)\n");
-	
 	g_ctrl = sig;
-	
-	// signal(SIGQUIT, SIG_DFL);
-	// printf("Quit (core dumped)\n");
+	printf("test\n");
+	// if (sig == SIGINT)
+	// 	rl_replace_line("", 0);
 }
+
+void	ft_ctrl_slash(int sig)
+{
+	// signal(SIGQUIT, SIG_DFL);
+	if (sig == SIGQUIT)
+		printf("Quit (core dumped)\n");
+	g_ctrl = sig;
+
+}
+
+void	manage_signal(int opt)
+{
+	if (opt == PARENT)
+	{
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, SIG_IGN);
+	}
+	else if (opt == CHILD)
+	{
+		signal(SIGQUIT, ft_ctrl_slash);
+		signal(SIGINT, ft_ctrl_c_child);
+	}
+}
+
+
 // int	ft_ctrl_d_heredoc(int sig)
 // {
 // 	if (line == NULL)
