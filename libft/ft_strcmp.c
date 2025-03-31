1f2d0a522c40e3a1b64ad515253cc6339c443c1d
Elisa Delannoy <elisadelannoy10@gmail.com>
2025-03-31 19:22:26 +0200

    strcmp

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: edelanno <edelanno@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:40:22 by nrontard          #+#    #+#             */
-/*   Updated: 2025/02/26 16:29:37 by edelanno         ###   ########.fr       */
+/*   Updated: 2025/03/31 19:22:04 by edelanno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i])
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
